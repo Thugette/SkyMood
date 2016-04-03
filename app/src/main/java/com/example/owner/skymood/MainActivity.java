d77e5d0a279ba7f3790c35f0e342054ab7d16060
Ivelina Golemanova <ivelinagolemanova@gmail.com>
2016-04-03 11:42:12 +0300

    MainActivity Demo Changes

--- app/src/main/java/com/example/owner/skymood/MainActivity.java
+++ app/src/main/java/com/example/owner/skymood/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.owner.skymood;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -12,6 +15,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -33,23 +37,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         city = (TextView) findViewById(R.id.cityNameTextView);
-        date = (TextView) findViewById(R.id.cityNameTextView);
-        temperature = (TextView) findViewById(R.id.cityNameTextView);
-        condition = (TextView) findViewById(R.id.cityNameTextView);
-        feelsLike = (TextView) findViewById(R.id.cityNameTextView);
-        lastUpdate = (TextView) findViewById(R.id.cityNameTextView);
+        date = (TextView) findViewById(R.id.dateAndTimeTextView);
+        temperature = (TextView) findViewById(R.id.temperatureTextView);
+        condition = (TextView) findViewById(R.id.conditionTextView);
+        feelsLike = (TextView) findViewById(R.id.feelsLikeTextView);
+        lastUpdate = (TextView) findViewById(R.id.lastUpdateTextView);
         weatherImage = (ImageView) findViewById(R.id.weatherImageView);
 
-
+        MyTask task = new MyTask();
+        task.execute();
 
     }
 
-    public void getLocation(View view) {
+  /*  public void getLocation(View view) {
         Intent intent = new Intent(MainActivity.this, AndroidLocationActivity.class);
         startActivity(intent);
-    }
+    } */
 
     class MyTask extends AsyncTask<Void, Void, Void> {
+
+        String dateAndTime;
+        String location;
+        String conditionn;
+        String iconUrl;
+        String temp;
+        Bitmap pic;
 
         @Override
         protected Void doInBackground(Void... params) {
@@ -70,6 +82,19 @@ public class MainActivity extends AppCompatActivity {
                 JSONObject forecast = (JSONObject) infoJson.get("forecast");
                 JSONObject simpleForecast = (JSONObject) forecast.get("simpleforecast");
                 JSONArray forecastDay = simpleForecast.getJSONArray("forecastday");
+                JSONObject forecastDayOne = (JSONObject) forecastDay.get(1);
+                JSONObject date = (JSONObject) forecastDayOne.get("date");
+                dateAndTime = date.getString("pretty");
+                location = date.getString("tz_long");
+                conditionn = date.getString("conditions");
+                Log.e("VVV","condition: " +  conditionn);
+                iconUrl = date.getString("icon_url");
+                JSONObject tempH = (JSONObject) date.get("high");
+                temp = tempH.getString("celsius");
+                Log.e("VVV", "temp: " + temp);
+
+                pic = BitmapFactory.decodeStream((InputStream) new URL(iconUrl).getContent());
+
 
             } catch (MalformedURLException e) {
                 e.printStackTrace();
@@ -80,6 +105,18 @@ public class MainActivity extends AppCompatActivity {
             }
 
             return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            city.setText(location);
+            date.setText(dateAndTime);
+            temperature.setText(temp);
+            condition.setText(conditionn);
+
+            weatherImage.setImageBitmap(pic);
+
+
         }
     }
 }
