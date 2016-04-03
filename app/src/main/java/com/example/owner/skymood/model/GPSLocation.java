462cefd88c59a113ac41859914555b067d81cb41
Thugette <didi_toleva@mail.bg>
2016-04-04 00:11:03 +0300

    dependencies addes, GPS location started

---
+++ app/src/main/java/com/example/owner/skymood/model/GPSLocation.java
@@ -0,0 +1,82 @@
+package com.example.owner.skymood.model;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+
+/**
+ * Created by owner on 03/04/2016.
+ */
+public class GPSLocation implements LocationListener {
+
+    Context context;
+
+    public GPSLocation(Context context) {
+        this.context = context;
+    }
+
+    private LocationManager locationManager = (LocationManager) context.getSystemService(context.LOCATION_SERVICE);
+//    if (context.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//        // TODO: Consider calling
+//        //    ActivityCompat#requestPermissions
+//        // here to request the missing permissions, and then overriding
+//        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//        //                                          int[] grantResults)
+//        // to handle the case where the user grants the permission. See the documentation
+//        // for ActivityCompat#requestPermissions for more details.
+//        return;
+//    }
+//    locationManager.requestLocationUpdates(context.LocationManager.GPS_PROVIDER, 1000, 1, this);
+
+    @Override
+    public void onLocationChanged(android.location.Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    private Location getLocation() {
+        String provider = LocationManager.GPS_PROVIDER;
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return null;
+        }
+        Location location = locationManager.getLastKnownLocation(provider);
+        return location;
+    }
+
+    public double getLatitude(){
+        return getLocation().getLatitude();
+    }
+
+    public double getLongitude(){
+        return getLocation().getLongitude();
+    }
+
+
+
+}
