922eb842b0af3ecac9d48e660eea8785c6fbae00
Ivelina Golemanova <ivelinagolemanova@gmail.com>
2016-04-05 16:27:47 +0300

    Listeners

--- app/src/main/java/com/example/owner/skymood/fragments/CurrentWeatherFragment.java
+++ app/src/main/java/com/example/owner/skymood/fragments/CurrentWeatherFragment.java
@@ -2,18 +2,15 @@ package com.example.owner.skymood.fragments;
 
 import android.Manifest;
 import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.location.Location;
 import android.location.LocationManager;
-import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -24,10 +21,10 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-import com.example.owner.skymood.HourlyActivity;
 import com.example.owner.skymood.R;
 import com.example.owner.skymood.location.NetworkLocationListener;
 
@@ -43,8 +40,10 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 
-public class CurrentWeatherFragment extends Fragment implements NetworkLocationListener.LocationReceiver, Swideable{
+public class CurrentWeatherFragment extends Fragment implements NetworkLocationListener.LocationReceiver, Swideable {
 
+    private ProgressBar progressBar;
+    private TextView chosenCity;
     private Spinner spinner;
     private ImageView sync;
     private ImageView gpsSearch;
@@ -62,6 +61,7 @@ public class CurrentWeatherFragment extends Fragment implements NetworkLocationL
     private double latitude;
     private double longtitude;
     String city;
+    String cityToDisplay;
 
     InputMethodManager keyboard;
     Context context;
@@ -69,7 +69,6 @@ public class CurrentWeatherFragment extends Fragment implements NetworkLocationL
     public CurrentWeatherFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -86,8 +85,11 @@ public class CurrentWeatherFragment extends Fragment implements NetworkLocationL
         feelsLike = (TextView) rootView.findViewById(R.id.feelsLikeTextView);
         lastUpdate = (TextView) rootView.findViewById(R.id.lastUpdateTextView);
         weatherImage = (ImageView) rootView.findViewById(R.id.weatherImageView);
+        chosenCity = (TextView) rootView.findViewById(R.id.chosenCity);
+        progressBar = (ProgressBar) rootView.findViewById(R.id.progressBar);
 
         ArrayList<String> cities =  new ArrayList<>();
+        cities.add("My Locations");
         cities.add("Sofia");
         cities.add("London");
         cities.add("Paris");
@@ -95,41 +97,12 @@ public class CurrentWeatherFragment extends Fragment implements NetworkLocationL
         ArrayAdapter adapter = new ArrayAdapter(context, android.R.layout.simple_spinner_dropdown_item, cities);
         spinner.setAdapter(adapter);
 
-        citySearch.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (writeCityEditText.getVisibility() == View.GONE) {
-                    //TODO: change animation
-                    Animation slide = new AnimationUtils().loadAnimation(getContext(), android.R.anim.fade_in);
-                    slide.setDuration(1000);
-                    writeCityEditText.startAnimation(slide);
-                    writeCityEditText.setVisibility(View.VISIBLE);
-                    writeCityEditText.setFocusable(true);
-                    writeCityEditText.requestFocus();
-                    keyboard = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
-                    keyboard.showSoftInput(writeCityEditText, 0);
-                } else {
-                    writeCityEditText.setVisibility(View.GONE);
-                    keyboard.hideSoftInputFromWindow(writeCityEditText.getWindowToken(), 0);
-                }
-            }
-        });
+        //spinner.setOnItemClickListener();
 
-        View.OnKeyListener onSearchPressed = new View.OnKeyListener() {
-            @Override
-            public boolean onKey(View v, int keyCode, KeyEvent event) {
-                if(keyCode == KeyEvent.KEYCODE_ENTER){
-                    getWeatherInfoByCity(writeCityEditText.getText().toString());
-                    keyboard.hideSoftInputFromWindow(writeCityEditText.getWindowToken(), 0);
-                    return true;
-                }
-                return false;
-            }
-        };
+        citySearch.setOnClickListener(new OnCitySearchClickListener());
 
+        View.OnKeyListener onSearchPressed = new OnSearchPressedListener();
         writeCityEditText.setOnKeyListener(onSearchPressed);
-
-
 
         //TODO: shared prefs
 
@@ -138,18 +111,18 @@ public class CurrentWeatherFragment extends Fragment implements NetworkLocationL
 
         //TODO: gps
 
-
         //for now by deafault will be sofia
-        city = "sofia";
+        setCity("Sofia");
+        chosenCity.setText(city);
         MyTask task = new MyTask();
         task.execute();
-
 
         return rootView;
     }
 
     public void setCity(String city){
         this.city = city.replace(" ", "_");
+        this.cityToDisplay = city.toUpperCase();
     }
 
     public void getWeatherInfoByCity(String city){
@@ -224,8 +197,7 @@ public class CurrentWeatherFragment extends Fragment implements NetworkLocationL
                 iconUrl = observation.getString("icon_url");
 
                 pic = BitmapFactory.decodeStream((InputStream) new URL(iconUrl).getContent());
-
-
+                
             } catch (MalformedURLException e) {
                 e.printStackTrace();
             } catch (IOException e) {
@@ -238,14 +210,56 @@ public class CurrentWeatherFragment extends Fragment implements NetworkLocationL
         }
 
         @Override
+        protected void onPreExecute() {
+            chosenCity.setVisibility(View.GONE);
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
         protected void onPostExecute(Void aVoid) {
-           // city.setText(location);
+            progressBar.setVisibility(View.GONE);
+            chosenCity.setVisibility(View.VISIBLE);
+            chosenCity.setText(cityToDisplay);
             temperature.setText(temp);
             condition.setText(conditionn);
             feelsLike.setText(feelsLikee);
 
             weatherImage.setImageBitmap(pic);
             weatherImage.setAdjustViewBounds(true);
+        }
+    }
+
+    private class OnSearchPressedListener implements View.OnKeyListener{
+
+        @Override
+        public boolean onKey(View v, int keyCode, KeyEvent event) {
+            if(keyCode == KeyEvent.KEYCODE_ENTER){
+                getWeatherInfoByCity(writeCityEditText.getText().toString());
+                keyboard.hideSoftInputFromWindow(writeCityEditText.getWindowToken(), 0);
+                return true;
+            }
+            return false;
+        }
+    }
+
+    private class OnCitySearchClickListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            if (writeCityEditText.getVisibility() == View.GONE) {
+                //TODO: change animation
+                Animation slide = new AnimationUtils().loadAnimation(getContext(), android.R.anim.fade_in);
+                slide.setDuration(1000);
+                writeCityEditText.startAnimation(slide);
+                writeCityEditText.setVisibility(View.VISIBLE);
+                writeCityEditText.setFocusable(true);
+                writeCityEditText.requestFocus();
+                keyboard = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+                keyboard.showSoftInput(writeCityEditText, 0);
+            } else {
+                writeCityEditText.setVisibility(View.GONE);
+                keyboard.hideSoftInputFromWindow(writeCityEditText.getWindowToken(), 0);
+            }
         }
     }
 
