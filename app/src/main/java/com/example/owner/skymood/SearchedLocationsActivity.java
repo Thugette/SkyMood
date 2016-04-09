5e3e2fc1695dcefc5df4634e6ea202ed71194a28
Ivelina Golemanova <ivelinagolemanova@gmail.com>
2016-04-09 11:00:26 +0300

    Toolbar

--- app/src/main/java/com/example/owner/skymood/SearchedLocationsActivity.java
+++ app/src/main/java/com/example/owner/skymood/SearchedLocationsActivity.java
@@ -3,45 +3,23 @@ package com.example.owner.skymood;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.CardView;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.example.owner.skymood.adapters.MyCardViewAdapter;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-
-public class MyLocationsActivity extends AppCompatActivity {
-
-    CardView cardView;
-    RecyclerView recycler;
-    ArrayList<String> data;
+public class SearchedLocationsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my_locations);
+        setContentView(R.layout.activity_searched_locations);
 
         //setting toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.tool_bar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
-
-        data = new ArrayList<>();
-        data.add("Sofia");
-        data.add("Plovdiv");
-        data.add("London");
-        this.cardView = (CardView) findViewById(R.id.mylocation_cardview);
-        this.recycler = (RecyclerView) findViewById(R.id.mylocation_recycler);
-        this.recycler.setLayoutManager(new LinearLayoutManager(this));
-        MyCardViewAdapter adapter = new MyCardViewAdapter(this, data);
-        recycler.setAdapter(adapter);
-
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- app/src/main/java/com/example/owner/skymood/SwipeViewActivity.java
+++ app/src/main/java/com/example/owner/skymood/SwipeViewActivity.java
@@ -1,22 +1,26 @@
 package com.example.owner.skymood;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.owner.skymood.adapters.CustomPagerAdapter;
 import com.example.owner.skymood.fragments.HourlyWeatherFragment;
 import com.example.owner.skymood.fragments.ICommunicatior;
 import com.example.owner.skymood.fragments.MoreInfoFragment;
 
-public class SwipeViewActivity extends FragmentActivity implements ICommunicatior{
+public class SwipeViewActivity extends AppCompatActivity implements ICommunicatior{
 
     public static final int NUMBER_OF_PAGES = 3;
 
     CustomPagerAdapter adapter;
     ViewPager pager;
-
 
     private String[] tabs = {"Current", "Hourly", "More info"};
 
@@ -25,10 +29,20 @@ public class SwipeViewActivity extends FragmentActivity implements ICommunicatio
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_swipe_view);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
         adapter = new CustomPagerAdapter(getSupportFragmentManager(), this);
         pager = (ViewPager) findViewById(R.id.pager);
         pager.setOffscreenPageLimit(3);
         pager.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 
     @Override
@@ -48,5 +62,26 @@ public class SwipeViewActivity extends FragmentActivity implements ICommunicatio
         ((HourlyWeatherFragment)fragment).setData(city, code);
         android.support.v4.app.Fragment fragment2 = adapter.getItem(2);
         ((MoreInfoFragment)fragment2).setInfo(city, code, date, min, max);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intent = null;
+        switch (item.getItemId()) {
+            case R.id.sky_mood:
+                intent = new Intent(this, SwipeViewActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.searched_locations:
+                intent = new Intent(this, SearchedLocationsActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.my_locations:
+                intent = new Intent(this, MyLocationsActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
